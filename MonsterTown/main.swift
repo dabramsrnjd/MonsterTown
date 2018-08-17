b1bc47ba1d9fc9473858c5473394dece84d94368
David Abrams <david@dhabramslaw.com>
2018-08-16 20:59:02 -0400

    Initial Commit

---
+++ MonsterTown/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MonsterTown
+//
+//  Created by David Abrams on 8/16/18.
+//  Copyright © 2018 David Abrams. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
